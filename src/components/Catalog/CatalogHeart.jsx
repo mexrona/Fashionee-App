f8cc621b99982e29026bac1742f02403ce239d3b
mexrona <tuichievamexrona@gmail.com>
2025-02-06 20:37:55 +0300

    Done addFavorite function

--- src/components/Catalog/CatalogHeart.jsx
+++ src/components/Catalog/CatalogHeart.jsx
@@ -1,11 +1,41 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+
+import {useLocalStorage} from "../../hooks/useLocalStorage";
 
 export default function CatalogHeart({addFavorite, product}) {
+    const {setLocalStorage, getLocalStorage} = useLocalStorage();
+
+    const getKey = getLocalStorage(`favorite${product.id}`);
+
     const [isActive, setIsActive] = useState(false);
 
     const handleClick = () => {
-        setIsActive(!isActive);
+        if (!getKey || getKey === "no-active") {
+            setLocalStorage(`favorite${product.id}`, "active");
+            setIsActive(!isActive);
+            return;
+        }
+
+        if (getKey === "active") {
+            setLocalStorage(`favorite${product.id}`, "no-active");
+            setIsActive(!isActive);
+        }
     };
+
+    useEffect(() => {
+        if (!getKey) {
+            return;
+        }
+
+        if (getKey === "active") {
+            setIsActive(true);
+            return;
+        }
+
+        if (getKey === "no-active") {
+            setIsActive(false);
+        }
+    }, []);
 
     return (
         <div
